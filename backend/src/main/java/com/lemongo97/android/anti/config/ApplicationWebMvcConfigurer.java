ec9c50257d2d448a45cc69f8561660836b1580c5
LemonGo97 <lemongo97@outlook.com>
2024-07-29 00:21:28 +0800

    解决刷新页面时的白屏问题

--- backend/src/main/java/com/lemongo97/android/anti/config/ApplicationWebMvcConfigurer.java
+++ backend/src/main/java/com/lemongo97/android/anti/config/ApplicationWebMvcConfigurer.java
@@ -2,8 +2,13 @@ package com.lemongo97.android.anti.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.List;
 
 @Configuration
 public class ApplicationWebMvcConfigurer implements WebMvcConfigurer {
@@ -12,5 +17,14 @@ public class ApplicationWebMvcConfigurer implements WebMvcConfigurer {
 		configurer.addPathPrefix("/api/", clazz -> clazz.isAnnotationPresent(RestController.class));
 	}
 
-
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+		resolvers.add((request, response, handler, e) -> {
+			if (e instanceof NoResourceFoundException){
+				return new ModelAndView("forward:/");
+			} else {
+				return null;
+			}
+		});
+	}
 }
