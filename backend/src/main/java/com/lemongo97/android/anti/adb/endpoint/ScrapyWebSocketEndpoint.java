05b8c1d1d16acfafaf7621361ce21e3ce3562c89
LemonGo97 <lemongo97@outlook.com>
2024-08-29 13:00:04 +0800

    ScrcpyWebsocket自动化与TODO补全

--- backend/src/main/java/com/lemongo97/android/anti/adb/endpoint/ScrapyWebSocketEndpoint.java
+++ backend/src/main/java/com/lemongo97/android/anti/adb/endpoint/ScrapyWebSocketEndpoint.java
@@ -2,6 +2,7 @@ package com.lemongo97.android.anti.adb.endpoint;
 
 import com.google.gson.Gson;
 import com.lemongo97.android.anti.annotation.WebSocketEndpoint;
+import com.lemongo97.android.anti.net.adb.ADBClient;
 import com.lemongo97.android.anti.net.scrcpy.ScrcpyClient;
 import com.lemongo97.android.anti.net.scrcpy.ScrcpyConfig;
 import com.lemongo97.android.anti.net.scrcpy.callback.IMediaMessageCallback;
@@ -10,6 +11,7 @@ import com.lemongo97.android.anti.services.AbstractWebSocketEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -58,11 +60,16 @@ public class ScrapyWebSocketEndpoint extends AbstractWebSocketEndpoint {
 		UriComponents uriComponents = UriComponentsBuilder.fromUri(session.getUri()).build();
 		MultiValueMap<String, String> params = uriComponents.getQueryParams();
 		sessionParams.put(sessionId, params);
-
-
-
-
-		// TODO 连接scrcpy
+		String deviceSerial = params.getFirst(ShellWebSocketEndpoint.Params.DEVICE);
+		ADBClient adbClient = new ADBClient();
+		adbClient.push(deviceSerial, ResourceUtils.getFile("classpath:" + "binary/scrcpy/scrcpy-server"), "/data/local/tmp/scrcpy-server.jar");
+		// TODO 检测本地端口是否占用，若占用，更换可用端口
+		adbClient.forward(deviceSerial, "tcp:27183", "localabstract:scrcpy");
+		// TODO 检测进程是否已被开启，若被开启，则关掉开启的进程
+		// TODO 根据用户的选择，判定是否开启音频流，然后根据判定生成 shell命令 和 ScrcpyClientConfig
+		adbClient.exec(deviceSerial, "cd /data/local/tmp && nohup app_process -Djava.class.path=scrcpy-server.jar /data/local/tmp com.genymobile.scrcpy.Server 2.6.1 tunnel_forward=true audio=false control=false > /dev/null &");
+		// TODO 将连接scrcpy的相关参数抽取
+		// TODO Scrcpy Client应接收ADB Server的Host和Port，而非开启的Scrcpy
 		ScrcpyClient scrcpyClient = new ScrcpyClient("127.0.0.1", 27183, ScrcpyConfig.ONLY_VIDEO, Collections.singleton(new ScrcpyMediaMessageCallback(session, this.gson)));
 		sessionADBStreamClientMap.put(sessionId, scrcpyClient);
 		scrcpyClient.connect();
@@ -95,6 +102,8 @@ public class ScrapyWebSocketEndpoint extends AbstractWebSocketEndpoint {
 			session.close(Optional.ofNullable(status).orElse(CloseStatus.NOT_ACCEPTABLE));
 			ScrcpyClient scrcpyClient = sessionADBStreamClientMap.get(sessionId);
 			scrcpyClient.disconnect();
+			// TODO 检测Android设备scrcpy进程是否仍在运行，若运行，则杀死进程
+			// TODO 删除 ADB Forward 规则
 		}catch (Exception e){
 			log.error("Error closing websocket session", e);
 		}finally {
